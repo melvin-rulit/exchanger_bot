36da8a2957cfbd142dab5aebd02bac5dbf70e7e9
melvin-rulit <marchenko1985g@outlook.com>
2025-03-23 14:05:52 +0000

    change

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 // const token = '10|Qn9ZdJufJ5htNcOnl971uFYLfOn2eYHHLSVzU1Rtc546c47c';
 // window.axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
-window.axios.defaults.baseURL = 'http://192.168.0.103:9000'
+//window.axios.defaults.baseURL = 'http://192.168.0.103:9000'
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.withCredentials = true;
+//window.axios.defaults.withCredentials = true;
 // axios.defaults.withXSRFToken = true;
