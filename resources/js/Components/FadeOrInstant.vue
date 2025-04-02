fb031bff44787ae533b2efbf032e261452e9377a
melvin-rulit <marchenko1985g@outlook.com>
2025-04-02 17:47:43 +0000

    компонент для эффектов появления и исчезновения

---
+++ resources/js/Components/FadeOrInstant.vue
@@ -0,0 +1,25 @@
+<template>
+  <component :is="transitionComponent">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    disableTransition: {
+      type: Boolean,
+      default: false,
+    },
+    name: {
+      type: String,
+      default: 'fade',
+    },
+  },
+  computed: {
+    transitionComponent() {
+      return this.disableTransition ? 'div' : 'transition';
+    },
+  },
+};
+</script>
