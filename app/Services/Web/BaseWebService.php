2ea61f482f73485da6971ea7ed0066a8b79c2151
melvin-rulit <marchenko1985g@outlook.com>
2025-04-29 15:23:53 +0000

    добавление дополнительного сервиса в базовый

--- app/Services/Web/BaseWebService.php
+++ app/Services/Web/BaseWebService.php
@@ -2,12 +2,13 @@
 
 namespace App\Services\Web;
 
+use App\Services\ClientService\ClientsService;
 use App\Services\RedisSessionService;
 use App\Services\TelegramBotService\TelegramMessageService;
 
 abstract class BaseWebService
 {
-    public function __construct(protected RedisSessionService $redis, protected TelegramMessageService $telegramMessageService){}
+    public function __construct(protected RedisSessionService $redis, protected TelegramMessageService $telegramMessageService, protected ClientsService $clientsService){}
 
     public function findOrThrow($modelClass, $id, $exceptionClass, $message = null)
     {
