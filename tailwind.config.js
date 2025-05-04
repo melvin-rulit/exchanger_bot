526cbe6b723e94b067d8e38db50560b64e57e40f
melvin-rulit <marchenko1985g@outlook.com>
2025-05-04 17:09:56 +0000

    анимация появления

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,15 @@ export default {
 
     theme: {
         extend: {
+            animation: {
+                'fade-in': 'fadeIn 0.2s ease-out',
+            },
+            keyframes: {
+                fadeIn: {
+                    '0%': { opacity: '0', transform: 'translateY(10px)' },
+                    '100%': { opacity: '1', transform: 'translateY(0)' },
+                },
+            },
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
