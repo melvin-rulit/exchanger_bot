9b0f2d6363ebf7cb4abd9730de69efe49545debb
melvin-rulit <marchenko1985g@outlook.com>
2025-05-06 13:11:40 +0000

    добавление файлов исключений

---
+++ app/Exceptions/Images/ImageDownloadException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions\Images;
+
+use Exception;
+
+class ImageDownloadException extends Exception
+
+{
+
+}
---
+++ app/Exceptions/Images/MediaLibraryException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Exceptions\Images;
+
+use Exception;
+
+class MediaLibraryException extends Exception
+
+{
+
+}
