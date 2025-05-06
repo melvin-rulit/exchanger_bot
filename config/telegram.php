59bb15957c3d9127af31ef462c199ea39c6fa9b6
melvin-rulit <marchenko1985g@outlook.com>
2025-05-06 09:32:50 +0000

    добавление ссылки для файлов

--- config/telegram.php
+++ config/telegram.php
@@ -3,6 +3,7 @@
 return [
     'telegram_bot' => [
         'api_url' => 'https://api.telegram.org/bot',
+        'api_file_url' => 'https://api.telegram.org/file/bot',
         'token' => env('TELEGRAM_BOT_TOKEN'),
     ]
 ];
