b5fa104f33c8dcba3ff88269ea3e7bb7e886af13
melvin-rulit <marchenko1985g@outlook.com>
2025-05-08 19:16:55 +0000

    добавление настроек для бота ошибок

--- config/telegram.php
+++ config/telegram.php
@@ -5,6 +5,11 @@ return [
         'api_url' => 'https://api.telegram.org/bot',
         'api_file_url' => 'https://api.telegram.org/file/bot',
         'token' => env('TELEGRAM_BOT_TOKEN'),
-    ]
+    ],
+    'error_notifier_bot' => [
+        'api_url' => 'https://api.telegram.org/bot',
+        'bot_token' => env('ERROR_NOTIFIER_BOT_TOKEN'),
+        'chat_id' => env('ERROR_NOTIFIER_CHAT_ID'),
+    ],
 ];
 
