3fffaa4b17904ba82dbc887311aafb441760b903
melvin-rulit <marchenko1985g@outlook.com>
2025-05-08 22:00:43 +0000

    дефолтное значение chanel

--- app/Helpers/Logs/Helpers.php
+++ app/Helpers/Logs/Helpers.php
@@ -2,17 +2,17 @@
 
 use App\Helpers\Logs\LogHelper;
 
-function log_info(string $message, array $context = [], string $channel = null): void
+function log_info(string $message, array $context = [], string $channel = 'telegram'): void
 {
   LogHelper::info($message, $context, $channel);
 }
 
-function log_error(string $message, array $context = [], string $channel = null): void
+function log_error(string $message, array $context = [], string $channel = 'telegram'): void
 {
     LogHelper::error($message, $context, $channel);
 }
 
-function log_debug(string $message, array $context = [], string $channel = null): void
+function log_debug(string $message, array $context = [], string $channel = 'telegram'): void
 {
     LogHelper::debug($message, $context, $channel);
 }
