2fe00030a88528810497ed07ab82c4691d1f68e3
melvin-rulit <marchenko1985g@outlook.com>
2025-05-08 22:08:45 +0000

    в базовом исключении есть возможность установить значение chanel

--- app/Exceptions/BaseReportableException.php
+++ app/Exceptions/BaseReportableException.php
@@ -11,11 +11,13 @@ abstract class BaseReportableException extends Exception
     use SendErrorsToTelegramTrait;
 
     protected array $extraData = [];
+    protected string $chanel = '';
 
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null, array $extraData = [])
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null, array $extraData = [], string $chanel = null)
     {
         parent::__construct($message, $code, $previous);
         $this->extraData = $extraData;
+        $this->chanel = $chanel;
     }
 
     /**
@@ -23,7 +25,7 @@ abstract class BaseReportableException extends Exception
      */
     public function report(): void
     {
-        log_error($this->getMessage(), $this->extraData, 'database');
+        log_error($this->getMessage(), $this->extraData, $this->chanel);
 
         $this->sendToTelegram($this->getMessage(), $this->extraData);
     }
--- app/Exceptions/Country/CountryBankNotFoundException.php
+++ app/Exceptions/Country/CountryBankNotFoundException.php
@@ -2,9 +2,10 @@
 
 namespace App\Exceptions\Country;
 
+use App\Exceptions\BaseReportableException;
 use Exception;
 
-class CountryBankNotFoundException extends Exception
+class CountryBankNotFoundException extends BaseReportableException
 
 {
 
