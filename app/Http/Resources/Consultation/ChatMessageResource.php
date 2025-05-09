df975898201e1a35b667d383d5c082e4c4eacae7
melvin-rulit <marchenko1985g@outlook.com>
2025-05-09 01:15:16 +0000

    добавил вывод пользователя в сообщении чата

---
+++ app/Http/Resources/Consultation/ChatMessageResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Consultation;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+use App\Http\Resources\User\UserResource;
+use App\Http\Resources\BaseTypedResource;
+
+class ChatMessageResource extends BaseTypedResource
+{
+    /**
+     * @extends BaseTypedResource<Message>
+     */
+    public function toArray(Request $request): array
+    {
+        /** @var Message $model */
+        $model = $this->getResource();
+
+        return [
+            'id'           => $model->id,
+            'message'      => $model->message,
+            'is_message'   => $model->is_message,
+            'is_close'     => $model->is_close,
+            'user'         => new UserResource($model->user),
+            'sender_type'  => $model->sender_type,
+            'image_url'    => $model->getImageUrl(),
+            'created_at'   => $model->created_at,
+        ];
+    }
+}
