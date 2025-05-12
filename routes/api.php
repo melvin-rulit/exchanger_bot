3c5a08aa4f0a5055d402c37279eff8e7f4095cfc
melvin-rulit <marchenko1985g@outlook.com>
2025-05-12 19:22:05 +0000

    роуты

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
-use App\Http\Controllers\TelegramController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExportController;
+use App\Http\Controllers\TelegramController;
 
 Route::get('/user', function (Request $request) {
     return $request;
 });
 
 Route::post('/webhook', [TelegramController::class, 'handleWebhook'])->name('webhook');
+Route::post('/export/telegram_service', [ExportController::class, 'exportTelegramService']);
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
-use App\Http\Controllers\CunsultationController;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Application;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TelegramController;
-use App\Http\Controllers\UserController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+use App\Http\Controllers\CunsultationController;
+use App\Http\Controllers\TemplateMessageController;
 
 Route::get('/', function () {
     return Inertia::render('Auth/Login', [
@@ -26,30 +27,42 @@ Route::get('/consultation', function () {
     return Inertia::render('Consultation/Index');
 })->middleware(['auth', 'verified'])->name('consultation');
 
+Route::get('/settings', function () {
+    return Inertia::render('Settings/Index');
+})->middleware(['auth', 'verified'])->name('settings');
+
 Route::middleware('auth')->group(function () {
 
-    Route::group(['prefix' => 'orders', 'name' => 'orders'], function () {
+    Route::group(['prefix' => 'orders', 'as' => 'orders.'], function () {
         Route::get('/', [OrderController::class, 'getOrders']);
         Route::patch('/{id}', [OrderController::class, 'setOrderMessage']);
-        Route::patch('/set_read_messages/{id}', [OrderController::class, 'setMessagesOrderRead']);
-        Route::patch('/get_order/{id}', [OrderController::class, 'getOrder'])->whereNumber('id');
-        Route::post('/send_message/{order}', [OrderController::class, 'storeMessage']);
-        Route::put('/update_order', [OrderController::class, 'updateOrder']);
+        Route::patch('/set_read_messages/{orderId}', [OrderController::class, 'setMessagesOrderRead'])->whereNumber('orderId');
+        Route::get('/get_order/{orderId}', [OrderController::class, 'getOrder'])->whereNumber('orderId');
+        Route::post('/send_message/{orderId}', [OrderController::class, 'storeMessage'])->whereNumber('orderId');
+        Route::put('/assign_executor', [OrderController::class, 'attachUserToOrder']);
         Route::put('/close_order', [OrderController::class, 'closeOrder']);
-        Route::put('/fix_order/{orderId}', [OrderController::class, 'fixOrder']);
+        Route::put('/fix_order/{orderId}', [OrderController::class, 'fixOrder'])->whereNumber('orderId');
+        Route::patch('/update_client_name/{orderId}', [OrderController::class, 'updateClientName'])->whereNumber('orderId');
+        Route::patch('/status/{orderId}', [OrderController::class, 'updateStatus'])->whereNumber('orderId');
     });
 
-    Route::group(['prefix' => 'consultation', 'name' => 'consultation'], function () {
+    Route::group(['prefix' => 'consultation', 'as' => 'consultation.'], function () {
         Route::get('/messages', [CunsultationController::class, 'getMessages']);
         Route::get('/today_messages/{message_id}', [CunsultationController::class, 'getTodayMessages']);
         Route::post('/send_message/{message_id}', [CunsultationController::class, 'storeMessage']);
         Route::patch('/set_read_messages/{message_id}', [CunsultationController::class, 'setMessagesOrderRead']);
     });
 
-    Route::group(['prefix' => 'users', 'name' => 'users'], function () {
+    Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
         Route::get('/', [UserController::class, 'getUsers']);
         Route::get('/managers', [UserController::class, 'getManagers']);
         Route::get('/me', [UserController::class, 'getAuthUser']);
+    });
+
+    Route::group(['prefix' => 'template', 'as' => 'templates.'], function () {
+        Route::get('/', [TemplateMessageController::class, 'getTemplates']);
+        Route::post('/message/add', [TemplateMessageController::class, 'storeTemplate']);
+        Route::delete('/message/{template_id}', [TemplateMessageController::class, 'deleteTemplate']);
     });
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
