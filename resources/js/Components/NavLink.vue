354eacc6421201a4d6552f9f7db966edc083d6e7
melvin-rulit <marchenko1985g@outlook.com>
2025-05-13 17:33:45 +0000

    стили для навигации

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -15,8 +15,9 @@ const props = defineProps({
 const classes = computed(() =>
     props.active
         ? 'inline-flex items-center text-xl font-bold dark:border-indigo-600 text-sm font-medium leading-5 text-white dark:text-gray-100 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400  hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out',
+        : 'inline-flex items-center pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400  hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out',
 );
+
 </script>
 
 <template>
