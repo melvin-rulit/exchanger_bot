f43f5cebdce4115e1611a1f35d5ca1e9c2c9e66d
melvin-rulit <marchenko1985g@outlook.com>
2025-05-13 17:29:45 +0000

    url в базовый сервис

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -4,10 +4,19 @@ namespace App\Services;
 
 use App\Models\Client;
 use App\Services\RedisService\RedisSessionService;
+use App\Exceptions\Helpers\InvalidStringValueException;
 
 abstract class BaseService
 {
-    public function __construct(protected RedisSessionService $redis){}
+    protected string $url;
+
+    /**
+     * @throws InvalidStringValueException
+     */
+    public function __construct(protected RedisSessionService $redis)
+    {
+        $this->url = ensure_string(config('telegram.telegram_bot.api_url'), 'telegram.telegram_bot.api_url') . ensure_string(config('telegram.telegram_bot.token'), 'telegram.telegram_bot.token');
+    }
 
     public function getClientByBotId($clientBotId)
     {
