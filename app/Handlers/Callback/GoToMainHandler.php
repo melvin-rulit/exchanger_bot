3743ebcf4ac7ecdd642f6a29b29b5657f86e9400
melvin-rulit <marchenko1985g@outlook.com>
2025-05-14 17:04:22 +0000

    Обработчик кнопки отмена из Меню получения реквизитов

--- app/Handlers/Callback/GoToMainHandler.php
+++ app/Handlers/Callback/GoToMainHandler.php
@@ -2,19 +2,30 @@
 
 namespace App\Handlers\Callback;
 
-use App\Services\RedisSessionService;
+use App\Exceptions\TelegramApiException;
 use App\Telegram\Keyboard\KeyboardFactory;
+use App\Services\OrderService\OrderService;
 use App\Services\ClientService\ClientsService;
+use App\Exceptions\Order\OrderNotFoundException;
+use App\Services\RedisService\RedisFieldService;
 use App\Services\TelegramBotService\TelegramMessageService;
 
 class GoToMainHandler
 {
-    public function __construct(protected ClientsService $clientsService,protected TelegramMessageService $telegramMessageService, protected RedisSessionService $redisSessionService) {}
+    public function __construct(protected ClientsService $clientsService, protected TelegramMessageService $telegramMessageService, protected RedisFieldService $redis, protected OrderService $orderService) {}
 
-    public function handle(int $clientBotId, int $chatId, int $messageId): void {
+    /**
+     * @throws TelegramApiException
+     * @throws OrderNotFoundException
+     */
+    public function handle(int $clientBotId, int $chatId, int $messageId): void
+    {
+        $this->orderService->changeStatus($this->redis->getOrderId($chatId), 'closed');
+        $this->clientsService->setClientMainInput($clientBotId, __('buttons.to_main'));
 
         $keyboard = KeyboardFactory::startKeyboard();
 
+        $this->telegramMessageService->deleteMessage($chatId, $messageId);
         $this->telegramMessageService->sendMessageWithButtons($chatId, __('messages.greeting'), $keyboard, $messageId);
     }
 }
