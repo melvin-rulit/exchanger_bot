662ca841e5d0360ed9de56f270350658e7bc8a39
melvin-rulit <marchenko1985g@outlook.com>
2025-05-15 20:14:49 +0000

    событие при закрытие заказа клиентом

---
+++ app/Events/OrderClosed.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Order;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class OrderClosed implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+
+    public Order $order;
+    public ?string $eventType;
+
+    public function __construct(Order $order, $eventType = null)
+    {
+        $this->order = $order;
+        $this->eventType = $eventType ?? 'default';
+    }
+
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('order_closed'),
+        ];
+    }
+    public function broadcastWith(): array
+    {
+        return [
+            'order' => [
+                'id' => $this->order->id
+            ],
+            'type' => $this->eventType ?? 'default',
+        ];
+    }
+    public function broadcastAs(): string
+    {
+        return 'order_closed';
+    }
+    public function shouldBroadcastNow(): bool
+    {
+        return true;
+    }
+}
