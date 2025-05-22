4728d2e61de5cbeac59ee6836e2f7b179a034fd9
melvin-rulit <marchenko1985g@outlook.com>
2025-05-22 11:14:47 +0000

    убрал ненужные методы и перенес в сервис

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -44,32 +44,9 @@ class Message extends Model implements HasMedia
         return $this->belongsTo(Client::class);
     }
 
-    public static function getTodayMessagesOnlyForConsultationByGroup(): LengthAwarePaginator
+    public static function getChatId($messageId)
     {
-        return self::whereIn('id', function ($query) {
-            $query->select(DB::raw('MAX(id)'))
-                ->from('messages')
-                ->whereDate('created_at', Carbon::now()->format('Y-m-d'))
-                ->whereNull('order_id')
-                ->where('sender_type', 'client')
-                ->groupBy('chat_id');
-        })
-            ->orderBy('created_at', 'desc')
-            ->paginate(16);
-    }
-
-    public static function getTodayMessagesOnlyForConsultationChat($chat_id): Collection
-    {
-        return self::where('chat_id', $chat_id)
-            ->whereNull('order_id')
-            ->whereDate('created_at', Carbon::today())
-            ->with('media')
-            ->get();
-    }
-
-    public static function getChatId($message_id)
-    {
-        return self::where('id', $message_id)
+        return self::where('id', $messageId)
             ->whereDate('created_at', Carbon::now()->format('Y-m-d'))
             ->value('chat_id');
     }
