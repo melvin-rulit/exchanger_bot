6097650837acf6ff09cae216176f0040c7963d04
melvin-rulit <marchenko1985g@outlook.com>
2025-05-25 19:08:55 +0000

    обсервер добавляет все настройки к пользователю

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,11 +3,18 @@
 namespace App\Observers;
 
 use App\Models\User;
+use App\Models\Setting;
 
 class UserObserver
 {
     public function created(User $user): void
     {
         $user->assignRole('Менеджер');
+
+        $settingIds = Setting::where('is_used', true)->pluck('id');
+
+        if ($settingIds->isNotEmpty()) {
+            $user->settings()->attach($settingIds);
+        }
     }
 }
