d2679f09a231a6243fa56e1e915e2e57103fb948
melvin-rulit <marchenko1985g@outlook.com>
2025-05-29 23:08:55 +0000

    файл загружающий все зависимости для главного файла

--- app/Telegram/Handlers/AbstractTelegramHandler.php
+++ app/Telegram/Handlers/AbstractTelegramHandler.php
@@ -2,27 +2,37 @@
 
 namespace App\Telegram\Handlers;
 
-use App\Handlers\ConsultationMessageHandler;
-use App\Handlers\LanguageMessageHandler;
-use App\Services\ActionService\CountryActionService;
-use App\Services\ChatService\ChatService;
-use App\Services\ClientService\ClientsService;
-use App\Services\CredentialService\CredentialService;
-use App\Services\OrderService\OrderService;
-use App\Services\RedisSessionService;
-use App\Services\TelegramBotService\TelegramMessageService;
-use App\Telegram\Route\CallbackMenuRouter;
-use App\Telegram\Route\CallbackRouter;
-use App\Telegram\Traits\HandlesImageDownload;
 use App\Telegram\Traits\SavePhoto;
+use App\Telegram\Traits\HandlesFile;
+use App\Telegram\Route\CallbackRouter;
+use App\Handlers\LanguageMessageHandler;
+use App\Services\ChatService\ChatService;
+use App\Telegram\Route\CallbackMenuRouter;
+use App\Handlers\RequisiteCallbackHandler;
+use App\Services\OrderService\OrderService;
+use App\Handlers\ConsultationMessageHandler;
+use App\Services\ClientService\ClientsService;
+use App\Telegram\Traits\LoadTelegramBotService;
+use App\Telegram\Route\CallbackMenuReturnRouter;
+use App\Services\RedisService\RedisFieldService;
+use App\Services\RedisService\RedisSessionService;
+use App\Services\ActionService\CountryActionService;
+use App\Services\CredentialService\CredentialService;
+use App\Exceptions\Helpers\InvalidStringValueException;
+use App\Services\TelegramBotService\TelegramMessageService;
 
 abstract class AbstractTelegramHandler
 {
-    use SavePhoto, HandlesImageDownload;
+    use SavePhoto, HandlesFile, LoadTelegramBotService;
 
     protected string $url;
+    protected string $download_url;
 
+    /**
+     * @throws InvalidStringValueException
+     */
     public function __construct(
+        protected StartHandles $startHandles,
         protected TelegramMessageService $telegramMessageService,
         protected ChatService $chatService,
         protected ClientsService $clientsService,
@@ -30,11 +40,17 @@ abstract class AbstractTelegramHandler
         protected CredentialService $credentialService,
         protected CountryActionService $countryActionService,
         protected RedisSessionService $redis,
+        protected RedisFieldService $redisField,
         protected ConsultationMessageHandler $consultationMessageHandler,
         protected LanguageMessageHandler $languageMessageHandler,
         protected CallbackRouter $callbackRouter,
-        protected CallbackMenuRouter $callbackMenuRouter
+        protected CallbackMenuRouter $callbackMenuRouter,
+        protected CallbackMenuReturnRouter $callbackMenuReturnRouter,
+        protected RequisiteCallbackHandler $requisiteCallbackHandler,
+        protected ConsultationHandles $consultationHandles,
+        protected ReceiptHandler $receiptHandler
     ) {
-        $this->url = ensure_string(config('telegram.telegram_bot.api_url')) . ensure_string(config('telegram.telegram_bot.token'));
+        $this->url = ensure_string(config('telegram.telegram_bot.api_url'), 'telegram.telegram_bot.api_url') . ensure_string(config('telegram.telegram_bot.token'), 'telegram.telegram_bot.token');
+        $this->download_url = ensure_string(config('telegram.telegram_bot.api_file_url')) . config('telegram.telegram_bot.token');
     }
 }
