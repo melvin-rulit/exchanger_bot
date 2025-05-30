7bd0d377c453b3faa1d1b0024bf667ce8932d226
melvin-rulit <marchenko1985g@outlook.com>
2025-05-30 19:26:31 +0000

    спинер загрузки страницы

---
+++ resources/js/Components/Spiner/Spiner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="loading-overlay" v-if="isLoading">
+    <pulse-loader></pulse-loader>
+  </div>
+</template>
+
+<script>
+
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    PulseLoader
+  },
+  props: {
+    isLoading: false
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style>
+.loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+}
+</style>
