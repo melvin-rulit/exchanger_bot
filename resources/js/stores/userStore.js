a699e8d3d0eeaabc9b470e0fd7ec677d85d4a002
melvin-rulit <marchenko1985g@outlook.com>
2025-06-03 16:05:52 +0000

    состояние для пользователя

---
+++ resources/js/stores/userStore.js
@@ -0,0 +1,47 @@
+import { defineStore } from 'pinia'
+
+export const useUserStore = defineStore('user', {
+    state: () => ({
+        users: [],
+        currentUser: null,
+    }),
+    getters: {
+
+    },
+    actions: {
+        setUser(userData) {
+            this.users = userData
+        },
+        setCurrentUser(userData) {
+            this.currentUser = userData
+        },
+        async fetchUser() {
+            try {
+                const response = await axios.get('/users')
+                this.setUser(response.data.data)
+            } catch (error) {
+                console.error('Не удалось получить пользователя:', error)
+            }
+        },
+        async getCurrentUser(id) {
+            if (!Array.isArray(this.users) || this.users.length === 0) {
+                await this.fetchUser()
+            }
+            return this.users.find(user => user.id === id) || null
+        },
+        async getUserLockPassword(id) {
+            if (!Array.isArray(this.users) || this.users.length === 0) {
+                await this.fetchUser()
+            }
+            const user = this.users.find(u => u.id === id)
+            return user ? user.lock_password : null
+        },
+        async getUserLockScreen(id) {
+            if (!Array.isArray(this.users) || this.users.length === 0) {
+                await this.fetchUser()
+            }
+            const user = this.users.find(u => u.id === id)
+            return user ? user.is_locked : null
+        }
+    },
+})
