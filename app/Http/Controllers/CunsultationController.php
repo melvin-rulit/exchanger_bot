eae3debd1648935899a07852d9fd0f6846cf76db
melvin-rulit <marchenko1985g@outlook.com>
2025-06-03 16:10:42 +0000

    выношу бизнес логику в отдельный слой

--- app/Http/Controllers/CunsultationController.php
+++ app/Http/Controllers/CunsultationController.php
@@ -2,58 +2,74 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ErrorResponse;
+use App\Http\Responses\SuccessResponse;
+use App\Http\Responses\NotFoundResponse;
+use App\Exceptions\TelegramApiException;
+use App\Exceptions\Order\OrderNotFoundException;
+use App\Exceptions\Images\MediaLibraryException;
+use App\Http\Resources\Consultation\MessageResource;
+use App\Http\Resources\Consultation\ChatMessageResource;
+use App\Services\Web\Consultation\ConsultationWebService;
+use App\Exceptions\Consultation\MessageNotFoundException;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use App\Http\Requests\Consultation\CreateConsultantMessageRequest;
-use App\Models\Message;
-use App\Services\TelegramBotService\TelegramMessageService;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
+use App\Http\Requests\Consultation\SetConsultantMessageReadRequest;
+use App\Http\Requests\Consultation\CreateConsultantMessageWithPhotoRequest;
 
 class CunsultationController extends Controller
 {
-    protected TelegramMessageService $telegramService;
-
-    public function __construct(TelegramMessageService $telegramService)
+    public function __construct(protected ConsultationWebService $consultationWebService){}
+    public function getMessages(): AnonymousResourceCollection
     {
-        $this->telegramService = $telegramService;
+            $messages = $this->consultationWebService->getTodayConsultationMessages();
+            return MessageResource::collection($messages);
     }
-    public function getMessages(): JsonResponse
-    {
-        $message = Message::getTodayMessagesOnlyForConsultationByGroup();
 
-        return new JsonResponse([
-            'last_message' => $message
-        ]);
+    public function getTodayMessages(int $message_id): NotFoundResponse|AnonymousResourceCollection
+    {
+        try {
+            $messages = $this->consultationWebService->getTodayMessagesChat($message_id);
+            return ChatMessageResource::collection($messages);
+
+        } catch (MessageNotFoundException $e) {
+            return new NotFoundResponse($e->getMessage());
+        }
     }
-    public function getTodayMessages(int $message_id): JsonResponse
-    {
-        $chat_id = Message::getChatId($message_id);
-        $messages = Message::getTodayMessagesOnlyForConsultation($chat_id);
 
-        return new JsonResponse([
-            'messages' => $messages
-        ]);
+    /**
+     * @throws TelegramApiException
+     */
+    public function storeMessage(CreateConsultantMessageRequest $request): ChatMessageResource|NotFoundResponse
+    {
+        try {
+            $message = $this->consultationWebService->storeMessage($request);
+            return new ChatMessageResource($message);
+
+        } catch (MessageNotFoundException $e) {
+            return new NotFoundResponse($e->getMessage());
+        }
     }
-    public function storeMessage(CreateConsultantMessageRequest  $request, $message_id): JsonResponse
+    public function storeMessageWithPhoto(CreateConsultantMessageWithPhotoRequest $request): NotFoundResponse|ErrorResponse|ChatMessageResource
     {
-        $chat_id = Message::getChatId($message_id);
+        try {
+            $message = $this->consultationWebService->storeMessageWithPhoto($request);
+            return new ChatMessageResource($message);
 
-        $created_message = Message::create([
-            'chat_id' => $chat_id,
-            'order_id' => null,
-            'user_id' => auth()->user()->id,
-            'sender_type' => 'user',
-            'message' => $request->getMessage(),
-        ]);
-
-        $this->telegramService->sendMessage($chat_id, $request->getMessage());
-
-        return response()->json($created_message);
+        } catch (MediaLibraryException $e) {
+            return new ErrorResponse('Не удалось сохранить изображение: ' . $e->getMessage());
+        } catch (OrderNotFoundException $e) {
+            return new NotFoundResponse($e->getMessage());
+        }
     }
-    public function setMessagesOrderRead($message_id): void
+    public function setMessagesConsultantRead(SetConsultantMessageReadRequest $request): SuccessResponse|NotFoundResponse
     {
-        $message = Message::find($message_id);
+        try {
+            $this->consultationWebService->setMessagesRead($request);
+            return new SuccessResponse('Сообщение отмечено прочитанным');
 
-        $message->is_message = true;
-        $message->save();
+        } catch (MessageNotFoundException $e) {
+            return new NotFoundResponse($e->getMessage());
+        }
     }
 }
