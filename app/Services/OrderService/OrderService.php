0728c89e53f2834c43e0a2b2c616f012c52cc4bf
melvin-rulit <marchenko1985g@outlook.com>
2025-06-03 16:20:33 +0000

    проработка сервиса

--- app/Services/OrderService/OrderService.php
+++ app/Services/OrderService/OrderService.php
@@ -2,41 +2,59 @@
 
 namespace App\Services\OrderService;
 
-use App\Events\NewOrder;
-use App\Events\UserMessageSent;
-use App\Models\Client;
-use App\Models\Currency;
 use App\Models\Order;
-use App\Models\Setting;
-use Illuminate\Support\Facades\Log;
-use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use App\Models\Client;
+use App\Services\BaseService;
+use App\Events\Order\NewOrder;
+use App\Telegram\Traits\HandlesFile;
+use App\Services\ClientService\ClientsService;
+use App\Handlers\Callback\WalletCallbackHandler;
+use App\Exceptions\Order\OrderNotFoundException;
+use App\Services\RedisService\RedisFieldService;
+use App\Services\RedisService\RedisSessionService;
+use App\Services\TelegramBotService\TelegramMessageService;
 
-class OrderService implements OrderServiceInterface
+class OrderService extends BaseService implements OrderServiceInterface
 {
-    public function saveOrder($chatId, $clientId, $amount, $currency_id)
+    use HandlesFile;
+
+    public string $download_url;
+    public function __construct(RedisSessionService $redis, protected ClientsService $clientsService, protected RedisFieldService $redisFieldService, protected TelegramMessageService $telegramMessageService, protected WalletCallbackHandler $walletCallbackHandler)
+    {
+        parent::__construct($redis);
+        $this->download_url = ensure_string(config('telegram.telegram_bot.api_file_url')) . config('telegram.telegram_bot.token');
+    }
+
+    public function saveOrder($chatId, $clientId, $amount)
     {
         $client = Client::where('bot_id', $clientId)->first();
-        $currency = Currency::find($currency_id);
 
         $order = Order::create([
             'chat_id' => $chatId,
             'client_id' => $client->id,
             'user_id' => null,
             'amount' => $amount,
-            'currency_name' => $currency->name,
         ]);
 
-        broadcast(new NewOrder($order));
-//        broadcast(new UserMessageSent($order));
+        $order->refresh();
+        broadcast(new NewOrder($order, 'new_order'));
+
         return $order;
     }
-    public function getOrdersForClient($clientId)
+
+    /**
+     * @throws OrderNotFoundException
+     */
+    public function changeStatus($orderId, $status)
     {
+        if (!$order = Order::find($orderId)) {
+            throw new OrderNotFoundException("Заказ с ID {$orderId} не найден.");
+        }
 
-    }
+        $order->status = $status;
+        $order->is_message = false;
+        $order->save();
 
-    public function setOrderStatus($orderId, $status)
-    {
-
+        return $order;
     }
 }
