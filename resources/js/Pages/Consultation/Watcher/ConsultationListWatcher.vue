dccad8824460b335444f62c059dfe59341a179c0
melvin-rulit <marchenko1985g@outlook.com>
2025-06-05 19:13:46 +0000

    добавил выключения звука если настройка включена notification

--- resources/js/Pages/Consultation/Watcher/ConsultationListWatcher.vue
+++ resources/js/Pages/Consultation/Watcher/ConsultationListWatcher.vue
@@ -1,20 +1,27 @@
 <script>
 import { usePusher } from '@/helpers/usePusher'
-import { ConsultationService } from '@/services/ConsultationService.js'
-import { useConsultationStore } from '@/stores/consultationStore'
 import { useSound } from '@/helpers/useSound'
+import { useUserStore } from '@/stores/userStore.js'
 import { eventBus } from '@/utils/eventBus.js'
+import { computed } from 'vue'
 
 export default {
   setup() {
     const { pusher } = usePusher()
     const { playSound } = useSound()
-    const consultationStore = useConsultationStore()
+    const userStore = useUserStore()
+
+    const notificationSettings = computed(() => {
+      return (userStore.currentUser?.settings?.find(s => s.key === 'notification') || { is_active: false })
+    })
 
     const subscribeToConsultation = () => {
       const channel = pusher.subscribe('consultation')
       channel.bind('new_message', async (data) => {
-        playSound('new_sms.mp3')
+        if (notificationSettings.value.is_active){
+          playSound('new_sms.mp3')
+        }
+
         try {
           eventBus.emit('newMessage')
         } catch (e) {
@@ -24,8 +31,9 @@ export default {
     }
 
     subscribeToConsultation()
-  },
 
+    return {userStore}
+  },
   render() {
     return null // Компонент невидимый
   }
