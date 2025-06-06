56ff4e2aed5db95299efb92cbfbe65a0c431bb19
melvin-rulit <marchenko1985g@outlook.com>
2025-06-06 10:56:10 +0000

    вернул store для консультации и сервис

--- resources/js/Pages/Consultation/Watcher/ConsultationListWatcher.vue
+++ resources/js/Pages/Consultation/Watcher/ConsultationListWatcher.vue
@@ -2,6 +2,8 @@
 import { usePusher } from '@/helpers/usePusher'
 import { useSound } from '@/helpers/useSound'
 import { useUserStore } from '@/stores/userStore.js'
+import { ConsultationService } from '@/services/ConsultationService.js'
+import { useConsultationStore } from '@/stores/consultationStore'
 import { eventBus } from '@/utils/eventBus.js'
 import { computed } from 'vue'
 
@@ -10,6 +12,7 @@ export default {
     const { pusher } = usePusher()
     const { playSound } = useSound()
     const userStore = useUserStore()
+    const consultationStore = useConsultationStore()
 
     const notificationSettings = computed(() => {
       return (userStore.currentUser?.settings?.find(s => s.key === 'notification') || { is_active: false })
@@ -18,6 +21,9 @@ export default {
     const subscribeToConsultation = () => {
       const channel = pusher.subscribe('consultation')
       channel.bind('new_message', async (data) => {
+        const response = await ConsultationService.getMessages('', 1)
+        consultationStore.setMessages(response.data.data)
+
         if (notificationSettings.value.is_active){
           playSound('new_sms.mp3')
         }
@@ -32,7 +38,7 @@ export default {
 
     subscribeToConsultation()
 
-    return {userStore}
+    return {userStore, consultationStore}
   },
   render() {
     return null // Компонент невидимый
