1de28f0f0c9b1605e4533cfd74fd05289af17af5
melvin-rulit <marchenko1985g@outlook.com>
2025-06-08 19:07:07 +0000

    запуск получения реквизитов

--- app/Actions/StartRequisiteAction.php
+++ app/Actions/StartRequisiteAction.php
@@ -2,54 +2,30 @@
 
 namespace App\Actions;
 
-use App\Models\Country;
 use App\DTO\RequisiteSelectionData;
 use App\Enums\TelegramCallbackAction;
+use App\Enums\Requisite\RequisiteField;
+use App\Exceptions\TelegramApiException;
 use App\Telegram\Keyboard\KeyboardFactory;
 use App\Services\ClientService\ClientsService;
+use App\Services\RedisService\RedisSessionService;
 use App\Services\TelegramBotService\TelegramMessageService;
 
 class StartRequisiteAction
 {
-    public function __construct(protected ClientsService $clientsService, protected TelegramMessageService $telegramMessageService) {}
+    public function __construct(protected ClientsService $clientsService, protected TelegramMessageService $telegramMessageService, protected RedisSessionService $redis) {}
 
+    /**
+     * @throws TelegramApiException
+     */
     public function execute(RequisiteSelectionData $data): void
     {
+        $this->redis->forgetRequisiteConsultant($data->chatId);
+        $this->clientsService->setRequisiteInput($data->clientBotId);
 
-        $language = $this->clientsService->getClientLanguage($data->clientBotId);
-        $this->clientsService->setUserCountryInput($data->clientBotId);
+        $keyboard['inline_keyboard'][] = KeyboardFactory::toConsultation(TelegramCallbackAction::ToConsultation->value. RequisiteField::REQUISITE->value);
+        $keyboard['inline_keyboard'][] = KeyboardFactory::toCancel(true);
+        $this->telegramMessageService->sendMessageWithButtons($data->chatId, __('messages.wait_requisite'), $keyboard, $data->messageId);
 
-        $countries = Country::where('is_used', true)->get();
-
-        $keyboard = [
-            'inline_keyboard' => [],
-        ];
-
-        $countryNameField = ($language === 'ru') ? 'name_ru' : 'name_en';
-        $row = [];
-
-        foreach ($countries as $index => $country) {
-            $row[] = [
-                'text' => ($country->flag ?? '') . ' ' . $country->$countryNameField,
-                'callback_data' => 'country_' . $country->code
-            ];
-
-            if (count($row) == 2) {
-                $keyboard['inline_keyboard'][] = $row;
-                $row = [];
-            }
-        }
-
-        if (count($row) > 0) {
-            $keyboard['inline_keyboard'][] = $row;
-        }
-
-        $keyboard['inline_keyboard'][] = KeyboardFactory::toBack(TelegramCallbackAction::SelectCountryBack);
-
-        $this->telegramMessageService->sendMessageWithButtons($data->chatId, __('messages.get_country'), $keyboard, $data->messageId);
-        $this->telegramMessageService->deleteMessage($data->chatId, $data->messageId );
-        $this->telegramMessageService->deleteMessage($data->chatId, $data->messageId -1);
-        $this->telegramMessageService->deleteMessage($data->chatId, $data->messageId -2);
     }
-
 }
