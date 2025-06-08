618d252c668801c4e06e4559cdae825e1c461ef6
melvin-rulit <marchenko1985g@outlook.com>
2025-06-08 18:53:23 +0000

    экшен для отправки города (прорпботать удаление сообщений)

--- app/Actions/StartCountryAction.php
+++ app/Actions/StartCountryAction.php
@@ -4,45 +4,66 @@ namespace App\Actions;
 
 use App\Models\Country;
 use App\DTO\CountrySelectionData;
-use App\Services\RedisSessionService;
+use App\Enums\Country\CountryField;
+use App\Enums\TelegramCallbackAction;
+use App\Exceptions\TelegramApiException;
 use App\Telegram\Keyboard\KeyboardFactory;
 use App\Services\ClientService\ClientsService;
+use App\Services\RedisService\RedisSessionService;
+use App\Exceptions\Country\CountryNotFoundException;
 use App\Services\TelegramBotService\TelegramMessageService;
 
 class StartCountryAction
 {
     public function __construct(protected ClientsService $clientsService, protected TelegramMessageService $telegramMessageService, protected RedisSessionService $redis) {}
+
+    /**
+     * @throws TelegramApiException
+     * @throws CountryNotFoundException
+     */
     public function execute(CountrySelectionData $data): void
     {
-        $country = Country::with('banks')->where('code', $data->countryCode)->first();
+        $this->redis->forgetCountryConsultant($data->chatId);
+        $language = $this->clientsService->getClientLanguage($data->clientBotId);
+        $this->clientsService->setUserCountryInput($data->clientBotId);
 
-        $this->redis->setSelectedCountry($data->chatId, $country->id);
-        $this->redis->setCountryCode($data->chatId, $data->countryCode);
+        $countries = Country::where('is_used', true)->get();
+
+        if ($countries->isEmpty()) {
+            throw new CountryNotFoundException("Ни одной страны is_used = true не найдено", 404, null, ['file', 'StartCountryAction:30'], 'database');
+        }
 
         $keyboard = [
-            'inline_keyboard' => []
+            'inline_keyboard' => [],
         ];
 
         $row = [];
 
-        foreach ($country->banks as $index => $bank) {
+        /** @var Country[] $countries */
+        foreach ($countries as $country) {
             $row[] = [
-                'text' => $bank->name,
-                'callback_data' => 'bank_' . $bank->id
+                'text' => ($country->flag ?? '') . ' ' . $country->getLocalizedName($language),
+                'callback_data' => 'country_' . $country->code
             ];
+
             if (count($row) == 2) {
                 $keyboard['inline_keyboard'][] = $row;
                 $row = [];
             }
         }
+
         if (count($row) > 0) {
             $keyboard['inline_keyboard'][] = $row;
         }
-        $keyboard['inline_keyboard'][] = KeyboardFactory::toBack('select_bank_back');
 
-        $this->clientsService->setClientBankInput($data->clientBotId);
+        $keyboard['inline_keyboard'][] = KeyboardFactory::toConsultation(TelegramCallbackAction::ToConsultation->value. CountryField::COUNTRY->value);
+        $keyboard['inline_keyboard'][] = KeyboardFactory::toBack(TelegramCallbackAction::SelectCountryBack);
 
+        $this->telegramMessageService->sendMessageWithButtons($data->chatId, __('messages.get_country'), $keyboard, $data->messageId);
+        $this->telegramMessageService->sendDeleteReplay($data->chatId);
+        //TODO проработать удаление сообщений
         $this->telegramMessageService->deleteMessage($data->chatId, $data->messageId);
-        $this->telegramMessageService->sendMessageWithButtons($data->chatId, __('messages.get_banks'), $keyboard, $data->messageId);
+        $this->telegramMessageService->deleteMessage($data->chatId, $data->messageId -1);
+        $this->telegramMessageService->deleteMessage($data->chatId, $data->messageId -2);
     }
 }
