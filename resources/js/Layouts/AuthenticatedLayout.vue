6080e95375f31f59cdb54f53ac59e1bc32c148c0
melvin-rulit <marchenko1985g@outlook.com>
2025-06-08 18:58:35 +0000

    убрал стили они уже в tailwind. добавил проверку на роли. пользователь берется из store. Компоненты для пользователя и админа

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="min-h-screen" style="background-image: url('Images/background.jpg'); background-size: cover; background-position: center;">
+    <div class="min-h-screen bg-main">
 
       <nav class="bg-transparent">
-        <!-- Primary Navigation Menu -->
+
         <div class="mx-auto with-custom">
           <div class="flex h-13 justify-between">
             <div class="flex">
@@ -10,39 +10,9 @@
                 <h1>{{ today }}</h1>
               </div>
 
-              <!-- Navigation Links -->
-              <div class="mt-2">
-                <div class="hidden space-x-2 sm:-my-px sm:ms-40 sm:flex">
-                  <div>
-                  <span v-if="ordersStore.unreadMessagesCount > 0"
-                        :class="['badge',route().current('dashboard') ? 'badge-active' : 'badge-inactive']">
-                        {{ ordersStore.unreadMessagesCount }}
-                  </span>
+                <ManagerLinks v-if="userStore.currentUser.role === 'Менеджер'"/>
+                <AdminLinks v-if="userStore.currentUser.role === 'Администратор'"/>
 
-                  </div>
-                  <NavLink
-                    :href="route('dashboard')"
-                    :active="route().current('dashboard')"
-                    :class="{'pulse-green': hasNewConsultMessages && !route().current('dashboard') && ordersStore.unreadMessagesCount > 0}">
-                    Заказы
-                  </NavLink>
-
-
-                  <div class="pl-5">
-                                    <span v-if="consultationStore.unreadMessagesCount > 0"
-                                          :class="['badge',route().current('consultation') ? 'badge-active' : 'badge-inactive']">
-                                          {{ consultationStore.unreadMessagesCount }}
-                                    </span>
-                  </div>
-                  <NavLink
-                    :href="route('consultation')"
-                    :active="route().current('consultation')"
-                    :class="{'pulse-green': hasNewConsultMessages && !route().current('consultation') && consultationStore.unreadMessagesCount > 0}">
-                    Консультация
-                  </NavLink>
-                </div>
-
-              </div>
             </div>
 
             <div class="sm:ms-6 sm:flex sm:items-center">
@@ -58,9 +28,9 @@
                     type="button" data-drawer-target="drawer-right-example"
                     data-drawer-show="drawer-right-example" data-drawer-placement="right"
                     aria-controls="drawer-right-example">{{ userStore.currentUser.name }}</span>
-              <span v-if="userStore.currentUser" class="d-none d-md-block">
+              <span v-if="userStore.currentUser && userStore.currentUser.role.length" class="d-none d-md-block">
                             <span class="px-2 py-0.5 text-xs font-bold bg-green-100 rounded-md shadow-md">
-                        {{ userStore.currentUser.role[0] }}
+                         {{ userStore.currentUser.role }}
                             </span>
               </span>
               <div class="relative group">
@@ -94,25 +64,18 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 import ApplicationLogo from '@/Components/ApplicationLogo.vue'
-import Dropdown from '@/Components/Dropdown.vue'
-import DropdownLink from '@/Components/DropdownLink.vue'
-import NavLink from '@/Components/NavLink.vue'
-import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue'
-import { Link } from '@inertiajs/vue3'
 import { Icon } from '@iconify/vue'
 import { router } from '@inertiajs/vue3'
 import { useOrdersStore } from '@/stores/ordersStore'
-import { UserService } from '@/services/UserService.js'
 import { useUserStore } from '@/stores/userStore.js'
 import { useConsultationStore } from '@/stores/consultationStore'
 import ConsultationListWatcher from '@/Pages/Consultation/Watcher/ConsultationListWatcher.vue'
 import { getFormattedDate } from '@/utils/dateFormatter.js'
-import { handleApiError } from '@/helpers/errors.js'
+import ManagerLinks from '@/Components/NavigationLinks/ManagerLinks.vue'
+import AdminLinks from '@/Components/NavigationLinks/AdminLinks.vue'
 
-const currentUser = ref(null)
-const errors = ref(null)
 const logout = () => {
   router.post(route('logout'))
 }
@@ -126,58 +89,18 @@ const ordersStore = useOrdersStore()
 const userStore = useUserStore()
 const consultationStore = useConsultationStore()
 
-async function getUser() {
-  try {
-    const response = await UserService.currentUser()
-    userStore.setCurrentUser(response.data.data)
-  } catch (error) {
-    errors.value = handleApiError(error)
-  }
-}
-
-onMounted(() => {
-  getUser()
-})
 </script>
 
 <style scoped>
+.bg-main {
+  background-image: url('/Images/background.jpg');
+  background-size: cover;
+  background-position: center;
+}
 .with-custom {
   width: 95%;
 }
-@keyframes pulse-text {
-  0%, 100% {
-    color: #6b7280;
-  }
-  50% {
-    color: #22c55e;
-    text-shadow: 0 0 0px #22c55e;
-  }
-}
-
-.pulse-green {
-  animation: pulse-text 1s infinite;
-}
-
 .date_block {
   color: ghostwhite;
-}
-.badge {
-  display: inline-block;
-  width: 18px;
-  height: 18px;
-  border-radius: 50%;
-  color: black;
-  font-size: 14px;
-  font-weight: bold;
-  text-align: center;
-  line-height: 18px;
-}
-
-.badge-active {
-  background-color: #d1d5db;
-}
-
-.badge-inactive {
-  background-color: #22c55e;
 }
 </style>
