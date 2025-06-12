d534ff133b6bb7b58dca3755a1be6e34033a555c
melvin-rulit <marchenko1985g@outlook.com>
2025-06-12 20:45:23 +0000

    ручка старта менюшки

--- app/Telegram/Route/CallbackMenuRouter.php
+++ app/Telegram/Route/CallbackMenuRouter.php
@@ -4,6 +4,8 @@ namespace App\Telegram\Route;
 
 use App\DTO\CallbackMenu;
 use Illuminate\Support\Facades\Log;
+use App\Exceptions\TelegramApiException;
+use App\Exceptions\Country\CountryNotFoundException;
 use App\Handlers\Callback\MenuHandlers\GetLanguageCallbackHandler;
 use App\Handlers\Callback\MenuHandlers\GetRequisiteCallbackHandler;
 use App\Handlers\Callback\MenuHandlers\GetConsultationCallbackHandler;
@@ -12,16 +14,19 @@ class CallbackMenuRouter
 {
     public function __construct(protected GetRequisiteCallbackHandler $getRequisiteCallbackHandler, protected GetConsultationCallbackHandler $getConsultationCallbackHandler, protected GetLanguageCallbackHandler $getLanguageCallbackHandler){}
 
+    /**
+     * @throws TelegramApiException|CountryNotFoundException
+     */
     public function route(CallbackMenu $callbackData, int $clientBotId, int $chatId, int $messageId): void
     {
         match ($callbackData->action) {
 
-            __('buttons.get_requisite') => $this->getRequisiteCallbackHandler->handle($clientBotId, $chatId, $messageId),
+            '💳 ' . __('buttons.get_requisite') => $this->getRequisiteCallbackHandler->handle($clientBotId, $chatId, $messageId),
             '👩‍💻 ' . __('buttons.consultation') => $this->getConsultationCallbackHandler->handle($clientBotId, $chatId, $messageId),
-            __('buttons.change_language') => $this->getLanguageCallbackHandler->handle($clientBotId, $chatId, $messageId),
+            '🌐 ' . __('buttons.change_language') => $this->getLanguageCallbackHandler->handle($clientBotId, $chatId, $messageId),
 
 
-            default => Log::warning('CallbackRouter: неизвестное действие', ['action' => $callbackData->action]),
+            default => Log::warning('CallbackMenuRouter: неизвестное действие', ['action' => $callbackData->action]),
         };
     }
 }
