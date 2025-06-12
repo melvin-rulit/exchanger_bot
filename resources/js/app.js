53cc5b6d660753b620f69d8f5148347e49a49c88
melvin-rulit <marchenko1985g@outlook.com>
2025-06-12 12:14:53 +0000

    поправка для прода

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,44 @@
+// import '../css/app.css';
+// import './bootstrap';
+//
+// import { createInertiaApp } from '@inertiajs/vue3';
+// import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+// import { createApp, h } from 'vue';
+// import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+// import { createPinia } from 'pinia'
+// import { useUserStore } from './stores/userStore';
+//
+// const pinia = createPinia()
+//
+// await createInertiaApp({
+//     title: (title) => `${title}`,
+//     resolve: (name) =>
+//         resolvePageComponent(
+//             `./Pages/${name}.vue`,
+//             import.meta.glob('./Pages/**/*.vue'),
+//         ),
+//     setup({ el, App, props, plugin }) {
+//         const vueApp = createApp({ render: () => h(App, props) });
+//
+//         vueApp.use(plugin).use(ZiggyVue).use(pinia);
+//
+//         const userStore = useUserStore();
+//         const initialUser = props.initialPage.props.auth.user || null
+//         if (initialUser) {
+//             initialUser.role = props.initialPage.props.auth.role
+//             userStore.setCurrentUser(props.initialPage.props.auth.user)
+//         }
+//         if (props.initialPage.props.auth?.user) {
+//             userStore.setUser(props.initialPage.props.auth.user);
+//         }
+//
+//         return vueApp.mount(el);
+//     },
+//     progress: {
+//         color: '#4B5563',
+//     },
+// });
+
 import '../css/app.css';
 import './bootstrap';
 
@@ -5,36 +46,40 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-import { createPinia } from 'pinia'
+import { createPinia } from 'pinia';
 import { useUserStore } from './stores/userStore';
 
-const pinia = createPinia()
+const pinia = createPinia();
 
-await createInertiaApp({
-    title: (title) => `${title}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        const vueApp = createApp({ render: () => h(App, props) });
+async function init() {
+    await createInertiaApp({
+        title: (title) => `${title}`,
+        resolve: (name) =>
+            resolvePageComponent(
+                `./Pages/${name}.vue`,
+                import.meta.glob('./Pages/**/*.vue'),
+            ),
+        setup({ el, App, props, plugin }) {
+            const vueApp = createApp({ render: () => h(App, props) });
 
-        vueApp.use(plugin).use(ZiggyVue).use(pinia);
+            vueApp.use(plugin).use(ZiggyVue).use(pinia);
 
-        const userStore = useUserStore();
-        const initialUser = props.initialPage.props.auth.user || null
-        if (initialUser) {
-            initialUser.role = props.initialPage.props.auth.role
-            userStore.setCurrentUser(props.initialPage.props.auth.user)
-        }
-        if (props.initialPage.props.auth?.user) {
-            userStore.setUser(props.initialPage.props.auth.user);
-        }
+            const userStore = useUserStore();
+            const initialUser = props.initialPage.props.auth.user || null
+            if (initialUser) {
+                initialUser.role = props.initialPage.props.auth.role
+                userStore.setCurrentUser(props.initialPage.props.auth.user)
+            }
+            if (props.initialPage.props.auth?.user) {
+                userStore.setUser(props.initialPage.props.auth.user);
+            }
 
-        return vueApp.mount(el);
-    },
-    progress: {
-        color: '#4B5563',
-    },
-});
+            return vueApp.mount(el);
+        },
+        progress: {
+            color: '#4B5563',
+        },
+    });
+}
+
+init();
