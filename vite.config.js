7a351a411553994597fe4b5cf0b4d4df1bc3e8bb
melvin-rulit <marchenko1985g@outlook.com>
2025-06-12 09:44:57 +0000

    vite build компилирует под новые стандарты

--- vite.config.js
+++ vite.config.js
@@ -3,32 +3,10 @@ import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-        cors: true,
-        origin: 'http://192.168.1.27:9000',
-        hmr: {
-            host: '192.168.1.27',
-        },
-        // proxy: {
-        //   '/api': {
-        //     target: 'http://192.168.0.103:9000',
-        //     changeOrigin: true,
-        //     secure: false
-        //   }
-        // }
-    },
-    // server: {
-    //     host: process.env.VITE_HOST || '127.0.0.1',
-    //     port: Number(process.env.VITE_PORT) || 5173,
-    //     cors: process.env.VITE_CORS === 'true',
-    //     origin: process.env.VITE_ORIGIN || 'http://127.0.0.1:9000',
-    //     hmr: {
-    //         host: process.env.VITE_HMR_HOST || 'localhost',
-    //     },
-    // },
     clearScreen: false,
+    esbuild: {
+        target: 'esnext',
+    },
     optimizeDeps: {
         exclude: ['<conflicting-package>'],
     },
@@ -46,4 +24,4 @@ export default defineConfig({
             },
         }),
     ],
-});
+})
