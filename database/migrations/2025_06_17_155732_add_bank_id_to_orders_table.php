9590658708ffa91e09fe9c92250fa3b5256121d3
melvin-rulit <marchenko1985g@outlook.com>
2025-06-17 13:06:40 +0000

    + банка для заказа

---
+++ database/migrations/2025_06_17_155732_add_bank_id_to_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('bank_id')->nullable()->after('user_id');
+            $table->foreign('bank_id')->references('id')->on('banks')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            if (Schema::hasColumn('orders', 'bank_id')) {
+                $table->dropForeign(['bank_id']);
+                $table->dropColumn('bank_id');
+            }
+        });
+    }
+};
