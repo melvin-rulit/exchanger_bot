0b6128f9b283e2681b6b6e76880ad0af085801fa
melvin-rulit <marchenko1985g@outlook.com>
2025-07-17 11:02:48 +0000

    обновление комента и имени клиента

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\Client\ClientNotFoundException;
 use App\Exceptions\Client\ClientsNotFoundException;
+use App\Exceptions\Order\OrderClientNotFoundException;
+use App\Http\Requests\Order\UpdateClientCommentRequest;
+use App\Http\Requests\Order\UpdateClientNameRequest;
 use App\Http\Responses\NotFoundResponse;
 use App\Http\Resources\Client\ClientResource;
+use App\Http\Responses\SuccessResponse;
 use App\Services\Web\Client\ClientWebService;
-use App\Exceptions\User\UsersNotFoundException;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class ClientController extends Controller
@@ -20,5 +24,25 @@ class ClientController extends Controller
     {
         $clients = $this->clientWebService->getClients();
         return ClientResource::collection($clients);
+    }
+
+    /**
+     * @throws ClientNotFoundException
+     */
+    public function updateClientComment(UpdateClientCommentRequest $request): SuccessResponse
+    {
+        $comment = $this->clientWebService->updateClientComment($request);
+        return new SuccessResponse('Комментарий успешно записан.', 'comment', ['comment' => $comment]);
+    }
+
+    public function updateClientName(UpdateClientNameRequest $request): NotFoundResponse|SuccessResponse
+    {
+        try {
+            $this->clientWebService->updateClientName($request);
+            return new SuccessResponse('Имя клиента успешно обновлено.');
+
+        } catch (OrderClientNotFoundException $e) {
+            return new NotFoundResponse($e->getMessage());
+        }
     }
 }
