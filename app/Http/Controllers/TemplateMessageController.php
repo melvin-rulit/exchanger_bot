cacbb10127996a1492096aeccdd71d3284d2cb7f
melvin-rulit <marchenko1985g@outlook.com>
2025-07-17 11:11:35 +0000

    шаблонные сообщения

--- app/Http/Controllers/TemplateMessageController.php
+++ app/Http/Controllers/TemplateMessageController.php
@@ -2,11 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Responses\ErrorResponse;
 use App\Http\Responses\SuccessResponse;
-use App\Http\Responses\NotFoundResponse;
 use App\Exceptions\AuthorizationException;
-use App\Http\Responses\ErrorCreateResponse;
 use App\Exceptions\Templates\TemplateCreateException;
 use App\Exceptions\Templates\TemplatesNotFoundException;
 use App\Http\Requests\Template\NewTemplateMessageRequest;
@@ -18,47 +15,37 @@ class TemplateMessageController extends Controller
 {
     public function __construct(protected TemplateMessageService $templateMessageService){}
 
-    public function getTemplates(): SuccessResponse|NotFoundResponse
+    public function getTemplates(): SuccessResponse
     {
-        try {
-            $messages = $this->templateMessageService->getTemplatesMessages();
-            return new SuccessResponse('Найденные шаблоны сообщений', 'template', ['messages' => $messages]);
-
-        } catch (TemplatesNotFoundException $e) {
-            return new NotFoundResponse($e->getMessage());
-        }
-    }
-    public function storeTemplate(NewTemplateMessageRequest $request): SuccessResponse|ErrorCreateResponse
-    {
-        try {
-            $messages = $this->templateMessageService->store($request);
-            return new SuccessResponse('Новый шаблон сохранен', 'template', ['messages' => $messages]);
-
-        } catch (TemplateCreateException $e) {
-            return new ErrorCreateResponse($e->getMessage());
-        }
-    }
-    public function updateTemplate(UpdateTemplateMessageRequest $request): SuccessResponse|NotFoundResponse
-    {
-        try {
-            $this->templateMessageService->update($request);
-            return new SuccessResponse('Шаблон обновлен');
-
-        } catch (TemplatesNotFoundException $e) {
-            return new NotFoundResponse($e->getMessage());
-        }
+        $messages = $this->templateMessageService->getTemplatesMessages();
+        return new SuccessResponse('Найденные шаблоны сообщений', 'template', ['messages' => $messages]);
     }
 
-    public function deleteTemplate(DeleteTemplateMessageRequest $request): SuccessResponse|NotFoundResponse|ErrorResponse
+    /**
+     * @throws TemplateCreateException
+     */
+    public function storeTemplate(NewTemplateMessageRequest $request): SuccessResponse
     {
-        try {
-            $messages = $this->templateMessageService->delete($request);
-            return new SuccessResponse('Шаблон удален', 'template', ['messages' => $messages]);
+        $messages = $this->templateMessageService->store($request);
+        return new SuccessResponse('Новый шаблон сохранен', 'template', ['messages' => $messages]);
+    }
 
-        } catch (TemplatesNotFoundException $e) {
-            return new NotFoundResponse($e->getMessage());
-        } catch (AuthorizationException $e) {
-            return new ErrorResponse('Недостаточно прав для удаления шаблона', 403);
-        }
+    /**
+     * @throws TemplatesNotFoundException
+     */
+    public function updateTemplate(UpdateTemplateMessageRequest $request): SuccessResponse
+    {
+        $this->templateMessageService->update($request);
+        return new SuccessResponse('Шаблон обновлен');
+    }
+
+    /**
+     * @throws TemplatesNotFoundException
+     * @throws AuthorizationException
+     */
+    public function deleteTemplate(DeleteTemplateMessageRequest $request): SuccessResponse
+    {
+        $messages = $this->templateMessageService->delete($request);
+        return new SuccessResponse('Шаблон удален', 'template', ['messages' => $messages]);
     }
 }
