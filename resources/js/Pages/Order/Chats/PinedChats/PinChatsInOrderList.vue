0af6e7d808cc224a43ff652e80ae436800621ee1
melvin-rulit <marchenko1985g@outlook.com>
2025-07-17 11:17:49 +0000

    уменьшение значков закрепленных чатов

--- resources/js/Pages/Order/Chats/PinedChats/PinChatsInOrderList.vue
+++ resources/js/Pages/Order/Chats/PinedChats/PinChatsInOrderList.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="relative w-[78px] h-[78px] cursor-pointer hover:text-gray-400 hover:shadow-lg" @click="handleClick">
+  <div class="relative w-[78px] h-[68px] cursor-pointer hover:text-gray-400 hover:shadow-lg" @click="handleClick">
     <span
       class="absolute top-1 right-0 z-10 text-xs rounded-full px-2 py-0.2 text-white"
       style="background-color: rgba(220, 38, 38, 0.8);">
       <span v-if="order?.client"> {{ order.client.first_name }} </span>
     </span>
-    <Icon icon="mdi:message-reply" width="76" height="76" class="absolute inset-0" />
+    <Icon icon="mdi:message-reply" width="76" height="66" class="absolute inset-0" />
     <div class="absolute inset-0 flex flex-col items-center justify-center text-md leading-tight text-black">
       <span v-if="order"> № {{ order.id }} </span>
     </div>
