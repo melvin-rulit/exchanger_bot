1c3f56a4b8be52003e69fe6ccf363ece96754ed8
melvin-rulit <marchenko1985g@outlook.com>
2025-07-17 11:03:22 +0000

    реквест зарытия чата

---
+++ app/Http/Requests/Consultation/closeChatRequest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Requests\Consultation;
+
+use App\Http\Requests\BaseRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class closeChatRequest extends BaseRequest
+{
+    /**
+     * Определяет, авторизован ли пользователь на выполнение запроса.
+     */
+    public function authorize(): bool
+    {
+        return auth()->check();
+    }
+
+    /**
+     * Laravel будет валидировать и данные из маршрута.
+     *
+     * @return array<string, string>
+     */
+    public function validationData(): array
+    {
+        return array_merge($this->all(), [
+            'chatId' => $this->route('chatId'),
+        ]);
+    }
+
+    /**
+     * Правила валидации для запроса.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * Кастомные сообщения об ошибках валидации.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'chatId.required' => 'chatId обязательный параметр',
+            'chatId.integer' => 'chatId должен быть целым числом',
+        ];
+    }
+
+    /**
+     * Чистое получение свойств.
+     */
+    public function getChatId(): int
+    {
+        return (int) $this->input('chatId');
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'errors' => $validator->errors(),
+        ], 422));
+    }
+}
