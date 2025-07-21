77b159fb73e79d17e76610e0adc8de23859cc53e
melvin-rulit <marchenko1985g@outlook.com>
2025-07-21 14:25:09 +0000

    расширил окно контента

--- resources/js/Pages/Order/Index.vue
+++ resources/js/Pages/Order/Index.vue
@@ -2,16 +2,8 @@
   <Head title="Заказы" />
 
   <AuthenticatedLayout :user="currentUser">
-<!--            <template #header>-->
-<!--                <h2-->
-<!--                    class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"-->
-<!--                >-->
-<!--                    Dashboard-->
-<!--                </h2>-->
-<!--            </template>-->
-
     <div class="py-2">
-      <div class="mx-auto max-w-9xl sm:px-6 lg:px-8">
+      <div class="mx-auto w-full lg:px-4">
         <div class="overflow-hidden  shadow-lg sm:rounded-lg dark:bg-gray-800">
           <div class="text-gray-900">
             <OrdersList></OrdersList>
