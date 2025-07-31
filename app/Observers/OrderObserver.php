36cd6d8bcf5b1dde5fca335548f5dd9f02cfc92e
melvin-rulit <marchenko1985g@outlook.com>
2025-07-31 10:14:16 +0000

    пока закоменчено теперь когда назначаешь себя ответственным то у других этот заказ пропадает

--- app/Observers/OrderObserver.php
+++ app/Observers/OrderObserver.php
@@ -9,8 +9,12 @@ class OrderObserver
 {
     public function updated(Order $order): void
     {
-        if ($order->isDirty('user_id') && $order->user_id !== auth()->id()) {
-            // 🔔 Событие вызывается при назначении менеджера заказу, чтобы обновить интерфейс на фронте (orderList)
+//        if ($order->isDirty('user_id') && $order->user_id !== auth()->id()) {
+//            // 🔔 Событие вызывается при назначении менеджера заказу, чтобы обновить интерфейс на фронте (orderList)
+//            event(new OrderUpdated($order, 'attach_user'));
+//        }
+
+        if ($order->isDirty('user_id')) {
             event(new OrderUpdated($order, 'attach_user'));
         }
 
